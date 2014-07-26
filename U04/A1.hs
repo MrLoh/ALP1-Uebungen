2b73a890c9e2bd61483b7e6d1a39b69e5293b20c
MrLoh <flojoto@hotmail.com>
2014-07-26 09:28:55 +0200

    finished U4 added PDF version

--- U04/A1.hs
+++ U04/A1.hs
@@ -1,23 +1,26 @@
-maximum :: [Int] -> Int
-maximum [] = error "no maximum of empty list"
-maximum [x] = x
-maximum (x:y:xs) | x>y       = max (x:xs)
-                 | otherwise = max (y:xs)
+--mymax :: (Double,Double,Double) -> Double
+--mymax (a,b,c) | a >= b && a >= c = a
+--              | b >= c           = b
+--              | otherwise        = c
 
-mymax :: (Double,Double,Double) -> Double
-mymax (a,b,c) | a >= b && a >= c = a
-              | b >= c           = b
-              | otherwise        = c
+--maximum :: [Double] -> Double
+--maximum [] = error "no maximum of empty list"
+--maximum [x] = x
+--maximum (x:y:xs) | x>y       = max (x:xs)
+--                 | otherwise = max (y:xs)
 
 rgb2cymk :: (Int,Int,Int) -> (Double,Double,Double,Double)
 rgb2cymk (0,0,0) = (0.0,0.0,0.0,1.0)
 rgb2cymk (r,g,b) = (c,m,y,k)
                    where
-                   w = mymax ((fromIntegral r)/255.0,(fromIntegral g)/255.0,(fromIntegral b)/255.0)
-                   c = (w-((fromIntegral r)/255.0))/w
-                   m = (w-((fromIntegral g)/255.0))/w
-                   y = (w-((fromIntegral b)/255.0))/w
+                   w = maximum [rd/255.0, gd/255.0, bd/255.0]
+                   c = (w-(rd/255.0))/w
+                   m = (w-(gd/255.0))/w
+                   y = (w-(bd/255.0))/w
                    k = 1.0-w
+                   rd = fromIntegral r
+                   gd = fromIntegral g
+                   bd = fromIntegral b
 
 main = do
  print( rgb2cymk (255,255,255) )
